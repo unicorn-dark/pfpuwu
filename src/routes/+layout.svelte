063fb7a7a74d015377bd5b7fb66741e3a68f3aa4
vs <vs@vss-MacBook-Pro.local>
2026-07-07 17:33:24 +0330

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
-    import '../app.css';
+    import "../app.css";
+    import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+    import { dev } from "$app/environment";
+    import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+    injectAnalytics({ mode: dev ? "development" : "production" });
+
+    injectSpeedInsights();
 </script>
+
 <slot />
